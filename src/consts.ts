8430ce8786d0894215df69e32e8e83d180f50f81
Jason Chen <js910924@gmail.com>
2024-09-27 17:29:18 +0800

    chore: update personal information

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "Chung Chih",
+  DESCRIPTION: "Welcome to my blog, a portfolio and blog for designers and developers.",
+  AUTHOR: "Jason Chen",
 }
 
 // Work Page
@@ -37,18 +37,18 @@ export const LINKS: Links = [
     TEXT: "Home", 
     HREF: "/", 
   },
-  { 
-    TEXT: "Work", 
-    HREF: "/work", 
+  {
+    TEXT: "Work",
+    HREF: "/work",
   },
   { 
     TEXT: "Blog", 
     HREF: "/blog", 
   },
-  { 
-    TEXT: "Projects", 
-    HREF: "/projects", 
-  },
+  // {
+  //   TEXT: "Projects",
+  //   HREF: "/projects",
+  // },
 ]
 
 // Socials
@@ -56,26 +56,26 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "js910924@gmail.com",
+    HREF: "mailto:js910924@gmail.com",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "js00000000",
+    HREF: "https://github.com/js00000000"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "Chen Chung Chih",
+    HREF: "https://www.linkedin.com/in/chung-chih-chen/",
   },
-  { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
+  // {
+  //   NAME: "Twitter",
+  //   ICON: "twitter-x",
+  //   TEXT: "",
+  //   HREF: "",
+  // },
 ]
 
